e3828a9202a38e422be19d74bb35021e1dd3e0f1
2taru <turianytsia.timur@gmail.com>
2024-07-22 16:32:45 +0300

    fix: fix contact page appearance on phone

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,12 +4,12 @@
         <div class="container px-5">
             <!-- Contact cards-->
             <div class="bg-light rounded-3 mb-3 py-5 px-4 px-md-5">
-                <div class="text-center mb-5">
+                <div class="text-center">
                     <h1 class="fw-bolder">Зв'яжись з нами</h1>
                 </div>
-                <div class="row gx-5 row-cols-2 row-cols-lg-4 py-5">
-                    <div class="col">
-                        <div class="feature bg-gradient text-dark rounded-3 mb-3">
+                <div class="row gx-5 row-cols-1 row-cols-lg-4 py-5">
+                    <div class="col mt-4">
+                        <div class="feature bg-gradient text-dark rounded-3 mb-1">
                             <i class="bi bi-telephone"></i>
                         </div>
                         <div class="h5">Зателефонуй нам</div>
@@ -19,22 +19,22 @@
                             <br> 050-909-0109
                         </p>
                     </div>
-                    <div class="col">
-                        <div class="feature bg-gradient text-dark rounded-3 mb-3">
+                    <div class="col mt-4">
+                        <div class="feature bg-gradient text-dark rounded-3 mb-1">
                             <i class="bi bi-chat-dots"></i>
                         </div>
                         <div class="h5 mb-2">Напиши на електронну пошту</div>
                         <p class="text-muted mb-0">svl.skytek@gmail.com</p>
                     </div>
-                    <div class="col">
-                        <div class="feature bg-gradient text-dark rounded-3 mb-3">
+                    <div class="col mt-4">
+                        <div class="feature bg-gradient text-dark rounded-3 mb-1">
                             <i class="bi bi-building"></i>
                         </div>
                         <div class="h5">Прийди в офіс</div>
                         <p class="text-muted mb-0">м. Мукачево вул. Августина Штефана, 25 Офіс 12, 2 поверх</p>
                     </div>
-                    <div class="col">
-                        <div class="feature bg-gradient text-dark rounded-3 mb-3">
+                    <div class="col mt-4">
+                        <div class="feature bg-gradient text-dark rounded-3 mb-1">
                             <i class="bi bi-question-circle"></i>
                         </div>
                         <div class="h5">Центр підтримки</div>
