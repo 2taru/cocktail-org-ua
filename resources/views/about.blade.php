bd4737c85613ba67a67c558feae46e6cfbf12437
2taru <turianytsia.timur@gmail.com>
2024-07-26 14:56:44 +0300

    feat: add information to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,49 +8,66 @@
             <div class="row justify-content-center">
                 <div class="col-lg-8 col-xxl-6">
                     <div class="text-center my-5">
-                        <h1 class="fw-bolder mb-3">Our mission is to make building websites easier for everyone.</h1>
-                        <p class="lead fw-normal text-muted mb-4">Start Bootstrap was built on the idea that quality,
-                            functional website templates and themes should be available to everyone. Use our open
-                            source, free products, or support us by purchasing one of our premium products or services.
+                        <h1 class="fw-bolder mb-3">Комплексні рішення для вашого успішного бізнесу від SKY TEK</h1>
+                        <p class="lead fw-normal text-muted mb-4">Вітаємо! Наша компанія SKY TEK була заснована у 2015
+                            році з метою спрощення ведення підприємницької діяльності шляхом надання всіх послуг для
+                            ведення бізнесу у одному місці.
+                            Ми прагнемо зробити управління вашим бізнесом легким та ефективним.
                         </p>
-                        <a class="btn btn-primary btn-lg" href="#scroll-target">Read our story</a>
+                        <a class="btn btn-outline-secondary btn-lg" href="#scroll-target">Більше про нас</a>
                     </div>
                 </div>
             </div>
         </div>
     </header>
     <!-- About section one-->
-    <section class="py-5 bg-light" id="scroll-target">
+    <section class="py-5 bg-light rounded-5 mx-3" id="scroll-target" style="--bs-bg-opacity: .3;">
         <div class="container px-5 my-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6"><img class="img-fluid rounded mb-5 mb-lg-0"
                         src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
                 <div class="col-lg-6">
-                    <h2 class="fw-bolder">Our founding</h2>
-                    <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae
-                        et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                    <h2 class="fw-bolder">Тепер ЦЕНТР БІЗНЕС ОБСЛУГОВУВАННЯ SKY TEK це:</h2>
+                    <ul class="list-unstyled mt-4 text-muted" style="font-size: 19px">
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> 5 офісів у Закарпатській
+                            області</li>
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> 227 послуг для успішного
+                            бізнесу в одному місці</li>
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> документи та обладнання для
+                            запуску бізнесу</li>
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> власне розроблене програмне
+                            забезпечення </li>
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> більше 30 співробітників в
+                            Україні та за кордоном</li>
+                        <li class="list-group-item mt-2"><i class="bi bi-caret-right"></i> послуги з електронного
+                            документообігу, бухгалтерії, системного адміністрування, реклами та дизайну, СММ просування,
+                            встановлення систем безпеки та відеонагляду, техніка для бізнесу, та інше.</li>
+                    </ul>
                 </div>
             </div>
         </div>
     </section>
     <!-- About section two-->
-    <section class="py-5">
+    <section class="py-5  rounded-5 mx-3">
         <div class="container px-5 my-5">
             <div class="row gx-5 align-items-center">
                 <div class="col-lg-6 order-first order-lg-last"><img class="img-fluid rounded mb-5 mb-lg-0"
                         src="https://dummyimage.com/600x400/343a40/6c757d" alt="..." /></div>
                 <div class="col-lg-6">
-                    <h2 class="fw-bolder">Growth &amp; beyond</h2>
-                    <p class="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae
-                        et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                    <h2 class="fw-bolder">Індивідуальний підхід та комплексні рішення</h2>
+                    <p class="lead fw-normal text-muted mb-0 mt-3">Ми підходимо до кожного клієнта індивідуально, враховуючи
+                        всі його потреби та побажання, і водночас пропонуємо комплексні рішення, які включають все
+                        необхідне для успішного ведення бізнесу.
+                    </p>
+                    <p class="lead fw-normal text-muted mb-0 mt-3">
+                        Ми завжди раді новим співпрацям та готові допомогти вам у досягненні ваших бізнес-цілей!
+                    </p>
                 </div>
             </div>
         </div>
     </section>
     <!-- Team members section-->
-    <section class="py-5 bg-light">
+    <section class="py-5 bg-light rounded-5 mx-3" style="--bs-bg-opacity: .3;">
         <div class="container px-5 my-5">
             <div class="text-center">
                 <h2 class="fw-bolder">Our team</h2>
