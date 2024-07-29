9c1e0694516a03d3bd39814814eba9cd8b4f82a1
2taru <turianytsia.timur@gmail.com>
2024-07-29 12:51:59 +0300

    feat: change font family

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -20,6 +20,11 @@
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Font Montserrat-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body class="d-flex flex-column h-100">
@@ -83,6 +88,13 @@
     <!-- Core theme JS-->
     <script src="js/scripts.js"></script>
     <style>
+        * {
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: <weight>;
+            font-style: normal;
+        }
+
         body {
             background: linear-gradient(-45deg, #e6e0b9, #c3e8da, #bde4e0, #e0c8e0, #e4bfd4);
             background-size: 400% 400%;
