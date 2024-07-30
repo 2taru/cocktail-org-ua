dedddebd7e1ea4fa874c3e7ebe1f340198d2de6d
2taru <turianytsia.timur@gmail.com>
2024-07-30 14:24:57 +0300

    style: change email message template

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -5,13 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $details['subject'] }}</title>
+    <title>{{ $details['subject'] }} | {{ $details['fromName'] }}</title>
 </head>
 
 <body>
-
-    <h4>{{ $details['subject'] }}</h4>
-    <p>{{ $details['message'] }}</p>
+    <h3>Тема: {{ $details['subject'] }}</h3>
+    <p><b>Повідомлення:</b> {{ $details['message'] }}</p>
+    <br/>
+    <h3>Надсилач:</h3>
+    <p><b>Ім'я:</b> {{$details['fromName']}}</p>
+    <p><b>Email:</b> {{$details['fromEmail']}}</p>
+    <p><b>Номер телефону:</b> {{$details['phone']}}</p>
 
 </body>
 
